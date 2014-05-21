21cc61f38a442699abcbcf3821734bfe15ab79ec
Jonathan Warner <jaxbot@gmail.com>
2014-05-21 19:35:16 -0400

    Make it a proper library

--- index.js
+++ index.js
@@ -1,51 +1,75 @@
-var xmpp = require('node-xmpp');
+var events = require('events');
 
-var config = require('./config');
+module.exports = function(username, password, onlineStatus) {
+	events.EventEmitter.call(this);
 
-var connection = new xmpp.Client(config);
+	var xmpp = require('node-xmpp');
+	var that = this;
 
-connection.on('online', function() {
-	console.log('online');
-	connection.send(new xmpp.Element('presence', {}).c('show').t('chat').up()
-		.c('status').t('Online!'));
-	
-	var roster_elem = new xmpp.Element('iq', { from: connection.jid, type: 'get', id: 'google-roster'})
-	.c('query', { xmlns: 'jabber:iq:roster', 'xmlns:gr': 'google:roster', 'gr:ext': '2' });
-	connection.send(roster_elem);
+	this.connection = connection = new xmpp.Client({
+		jid: username,
+		password: password,
+		host: "talk.google.com"
+	});
 
-	/*
-	connection.socket.setTimeout(0);
-	connection.socket.setKeepAlive(true, 10000);
-	*/
+	this.connection.on('online', function() {
+		connection.send(new xmpp.Element('presence', {})
+			.c('show')
+			.t('chat')
+			.up()
+			.c('status')
+			.t(onlineStatus || 'Online')
+		);
+		
+		var roster_elem = new xmpp.Element('iq', {
+			'from': connection.jid,
+			'type': 'get',
+			'id': 'google-roster'
+		}).c('query', {
+			'xmlns': 'jabber:iq:roster',
+			'xmlns:gr': 'google:roster',
+			'gr:ext': '2'
+		});
 
-});
+		connection.send(roster_elem);
 
-connection.on('stanza', function(stanza) {
-	if (stanza.is('message') && (stanza.attrs.type !== 'error')) {
-		stanza.attrs.to = stanza.attrs.from;
-		delete stanza.attrs.from;
+		that.emit('online');
+	});
 
-		connection.send(stanza);
+	this.connection.on('stanza', function(stanza) {
+		if (stanza.is('message') && (stanza.attrs.type !== 'error') && (stanza.getChildText('body'))) {
+			that.emit('message',
+				stanza.attrs.from,
+				stanza.getChildText('body')
+			);
+		}
 
-		console.log(stanza.getChildText('body'));
+		if(stanza.is('presence') && stanza.attrs.type === 'subscribe') {
+			stanza.attrs.to = stanza.attrs.from;
+			delete stanza.attrs.from;
 
+			connection.send(stanza);
+		}
+	});
+
+	this.connection.on('error', function(e) {
+		console.log(e);
+	});
+
+	this.sendMessage = function(to, message) {
+		var stanza = new xmpp.Element('message',
+			{
+				to: to,
+				type: 'chat'
+			})
+			.c('body')
+			.t(message);
+
+		this.connection.send(stanza);
 	}
-	if(stanza.is('presence') && stanza.attrs.type === 'subscribe') {
-		stanza.attrs.to = stanza.attrs.from;
-		delete stanza.attrs.from;
 
-		connection.send(stanza);
-	}
-});
-
-connection.on('error', function(e) {
-	console.log(e);
-});
-
-function sendMessage(to, message) {
-	var stanza = new xmpp.Element(
-		'message',
-		{ to: to, type: 'chat' }
-	).c('body').t(message")
-	connection.send(stanza);
+	return this;
 }
+
+module.exports.prototype.__proto__ = events.EventEmitter.prototype;
+
