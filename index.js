a9a36734d97b9f996c7900101658d5434de3c2a7
Jonathan Warner <jaxbot@gmail.com>
2014-05-20 23:00:22 -0400

    Cleanup

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ connection.on('online', function() {
 });
 
 connection.on('stanza', function(stanza) {
-	console.log(stanza);
 	if (stanza.is('message') && (stanza.attrs.type !== 'error')) {
 		stanza.attrs.to = stanza.attrs.from;
 		delete stanza.attrs.from;
